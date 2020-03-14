b051598ba2a7ec0b44069545abc3abb476a091fc
wickeddreamer <55858312+wickeddreamer@users.noreply.github.com>
2020-03-13 18:52:43 -0700

    Update TTS_Client.java

--- src/TTS_Client.java
+++ src/TTS_Client.java
@@ -1,42 +1,53 @@
+import java.awt.*;
 import java.io.*;
-import java.util.Scanner;
-import marytts.modules.synthesis.Voice;
+import javafx.scene.control.TextArea;
 
 public class TTS_Client extends T_C_P {
     private TextToSpeech tts = new TextToSpeech();
     private String text;
+    private boolean voiceOn = true;
 
     public TTS_Client(String serverAddress, int serverPort) throws Exception {
         super(serverAddress, serverPort);
         tts.setVoice("cmu-rms-hsmm");
-
     }
 
-    //implements abstract method recieveMessage
-    public void recieveMessage() throws IOException {
+    // implements abstract method receiveMessage
+    @Override
+    public void receiveMessage(TextArea view) throws IOException {
         while (true) {
             try {
-                //InputStream, represents an ordered stream of bytes.
-                //In other words, you can read data from a Java InputStream as an ordered sequence of bytes.
+                // InputStream, represents an ordered stream of bytes.
+                // In other words, you can read data from a Java InputStream as an ordered
+                // sequence of bytes.
                 InputStream rec = this.socket.getInputStream();
-                // thereby turning the byte based InputStream into a character based Reader. In other words,
-                //the Java InputStreamReader interprets the bytes of an InputStream as text instead of numerical data.
+                // thereby turning the byte based InputStream into a character based Reader. In
+                // other words,
+                // the Java InputStreamReader interprets the bytes of an InputStream as text
+                // instead of numerical data.
                 InputStreamReader reader = new InputStreamReader(rec);
                 int character;
-//The String object is immutable. Every time you use one of the methods in the System.String class,
-                //you create a new string object in memory,
-                //The System.Text.StringBuilder class can be used when you want to modify a string without creating a new object.
+                //The String object is immutable. Every time you use one of the methods in the System.String class,
+                // you create a new string object in memory,
+                // The System.Text.StringBuilder class can be used when you want to modify a
+                // string without creating a new object.
                 StringBuilder data = new StringBuilder();
                 // this was -1 before...no idea
-                //read return char as int which contains the char value of the char read.
+                // read return char as int which contains the char value of the char read.
                 while ((character = reader.read()) != 0) {
-                    //converts the char array message sent by server to a string
-                    //cast character as char..converting the int byte equvialent to a readable char
+                    // converts the char array message sent by server to a string
+                    // cast character as char..converting the int byte equvialent to a readable char
                     data.append((char) character);
                 }
-                text= data.toString();
-                tts.speak(text, 2.0f, false, true);
-
+                text = data.toString();
+                //only speaks if the feature is enabled
+                if (voiceOn == true) {
+                    tts.speak(text, 2.0f, false, true);
+                }
+                //appends the new message to the TextArea screen
+                content = view.getText() + "\n" + text;
+                view.setText(content);
+                Toolkit.getDefaultToolkit().beep();
                 System.out.print(data);
             } catch (Exception e) {
                 System.out.println(e);
@@ -45,5 +56,9 @@ public class TTS_Client extends T_C_P {
                 return;
             }
         }
+    }
+
+    public void setVoiceOn(boolean status) {
+        voiceOn = status;
     }
 }

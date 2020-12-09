5ff1eb19aa74d472b395f79cca665d164e358cf7
Richard Forrester <55858312+RichardAF8@users.noreply.github.com>
2020-12-08 19:49:01 -0800

    Update T_C_P.java

--- src/T_C_P.java
+++ src/T_C_P.java
@@ -1,69 +1,88 @@
-import java.awt.*;
-import java.io.*;
 import javafx.scene.control.TextArea;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.locks.ReentrantLock;
 
-public class TTS_Client extends T_C_P {
-    private TextToSpeech tts = new TextToSpeech();
-    private String text;
-    private boolean voiceOn = true;
-
-    // Client with Text-To-Speech capability
-    public TTS_Client(String serverAddress, int serverPort) throws Exception {
-        super(serverAddress, serverPort);
-        tts.setVoice("cmu-rms-hsmm");
-    }
-
-    // implements abstract method receiveMessage
-    @Override
-    public void receiveMessage(TextArea view) throws IOException {
-        while (true) {
-            try {
-                // InputStream, represents an ordered stream of bytes.
-                // In other words, you can read data from a Java InputStream as an ordered
-                // sequence of bytes.
-                InputStream rec = getInputStream();
-                // thereby turning the byte based InputStream into a character based Reader. In
-                // other words,
-                // the Java InputStreamReader interprets the bytes of an InputStream as text
-                // instead of numerical data.
-                InputStreamReader reader = new InputStreamReader(rec);
-                int character;
-                // The String object is immutable. Every time you use one of the methods in the
-                // System.String class,
-                // you create a new string object in memory,
-                // The System.Text.StringBuilder class can be used when you want to modify a
-                // string without creating a new object.
-                StringBuilder data = new StringBuilder();
-                // this was -1 before...no idea
-                // read return char as int which contains the char value of the char read.
-                while ((character = reader.read()) != 0) {
-                    // converts the char array message sent by server to a string
-                    // cast character as char..converting the int byte equvialent to a readable char
-                    data.append((char) character);
-                }
-                text = data.toString();
+//Abstract class for TCP client
+public abstract class T_C_P {
+    private Socket socket;
+    private String content;
+    private ReentrantLock lock = new ReentrantLock();
+    private BlockingQueue<String> queue = new ArrayBlockingQueue<>(10);
 
 
-                // appends the new message to the TextArea screen
-                updateTextField(text, view);
-
-                //only speaks if the feature is enabled
-                if (voiceOn == true) {
-                    tts.speak(text, 2.0f, false, true);
-                }
-
-                // Beeps for effect XD
-                Toolkit.getDefaultToolkit().beep();
-            } catch (Exception e) {
-                System.out.println(e);
-                closeSocket();
-                System.out.println("Server Closed");
-                return;
-            }
+    // connects to the listening socket of the server
+    public T_C_P(String serverAddress, int serverPort) throws Exception {
+        try {
+            this.socket = new Socket(serverAddress, serverPort);
+        } catch (Exception e) {
+            System.out.println(e);
         }
     }
 
-    public void setVoiceOn(boolean status) {
-        voiceOn = status;
+    // sends message
+    public void sendMessage(String message, TextArea view) throws IOException {
+        try {
+            // appends TextField
+
+            updateTextField("Me:\n"+message, view);
+
+            // Sends message to the server
+            PrintWriter out = new PrintWriter(this.socket.getOutputStream(), true);
+            out.println(message);
+            out.flush();
+
+        } catch (Exception e) {
+            System.out.println(e);
+            socket.close();
+            System.out.println("Server Closed");
+            return;
+        }
     }
+
+    public void updateTextField(String message, TextArea view) {
+
+        // prevents concurrent string modification while updating TextField on the G.U.I
+        lock.lock();
+        content = view.getText() + "\n" + message;
+        view.setText(content);
+        lock.unlock();
+    }
+
+    public void addToQueue(String item) {
+        try {
+            queue.put(item);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    public String removeFromQueue() {
+        try {
+            return queue.take();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        return "Message cannot be loaded";
+    }
+
+    public InputStream getInputStream() throws IOException {
+        return this.socket.getInputStream();
+    }
+
+    public void closeSocket() throws IOException {
+        socket.close();
+    }
+
+    public InetAddress getInetAddress() {
+        return socket.getInetAddress();
+    }
+
+    // waits for message from the server
+    public abstract void receiveMessage(TextArea view) throws IOException;
 }
